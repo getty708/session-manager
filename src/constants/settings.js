6c85dc06790e3815a1ca1994264a5a5f0ac97491
Yoshimura Naoya <yesyes708@gmail.com>
2017-12-15 10:11:59 +0900

    Add object detection settings

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,14 +1,28 @@
 // Gesture Set
+// export const GESTURE_SET = [
+//   {id: 0, name: 'air tap',        img: "./img/gestures/air_tap.jpg"},
+//   {id: 1, name: 'flick left',     img: "./img/gestures/flick_left.jpg"},
+//   {id: 2, name: 'flick right',    img: "./img/gestures/flick_right.jpg"},
+//   {id: 3, name: 'pinch in',       img: "./img/gestures/pinch_in.jpg"},
+//   {id: 4, name: 'pinch out',      img: "./img/gestures/pinch_out.jpg"},
+//   {id: 5, name: 'tap arm',        img: "./img/gestures/tap_arm.jpg"},
+//   {id: 6, name: 'tap left',       img: "./img/gestures/tap_left.jpg"},
+//   {id: 7, name: 'tap sensor',     img: "./img/gestures/tap_sensor.jpg"},
+//   {id: 8, name: 'tap right',      img: "./img/gestures/tap_right.jpg"},
+//   {id: 9, name: 'tap hand',      img: "./img/gestures/tap_hand.jpg"},
+// ]
+
+// Objects
 export const GESTURE_SET = [
-  {id: 0, name: 'air tap',        img: "./img/air_tap.jpg"},
-  {id: 1, name: 'flick left',     img: "./img/flick_left.jpg"},
-  {id: 2, name: 'flick right',    img: "./img/flick_right.jpg"},
-  {id: 3, name: 'pinch in',       img: "./img/pinch_in.jpg"},
-  {id: 4, name: 'pinch out',      img: "./img/pinch_out.jpg"},
-  {id: 5, name: 'one finger rub', img: "./img/one_finger_rub.jpg"},
-  {id: 6, name: 'tap arm',        img: "./img/tap_arm.jpg"},
-  {id: 7, name: 'tap left',       img: "./img/tap_left.jpg"},
-  {id: 8, name: 'tap sensor',     img: "./img/tap_sensor.jpg"},
-  {id: 9, name: 'tap right',      img: "./img/tap_right.jpg"},
-  {id: 10, name: 'tap hand',      img: "./img/tap_hand.jpg"},
+  {id: 0, name: 'drill',        img: "./img/objects/drill.jpg"},
+  {id: 1, name: 'driver',     img: "./img/objects/driver.jpg"},
+  {id: 2, name: 'hair dryer',    img: "./img/objects/hair_dryer.jpg"},
+  {id: 3, name: 'hand cleaner',       img: "./img/objects/hand_cleaner.jpg"},
+  {id: 4, name: 'massager (fast)',      img: "./img/objects/massager_fast.jpg"},
+  {id: 5, name: 'massager (slow)',        img: "./img/objects/massager_slow.jpg"},
+  {id: 6, name: 'toothbrush',       img: "./img/objects/toothbrush.jpg"},
+  {id: 7, name: 'shaver',     img: "./img/objects/shaver.jpg"},
+  {id: 8, name: 'fun',      img: "./img/objects/fun.jpg"},
+  {id: 9, name: 'hand mixer',      img: "./img/objects/hand_mixer.jpg"},
+  {id:10, name: 'blender',      img: "./img/objects/blender.jpg"}
 ]
