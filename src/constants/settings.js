d98a233705c1f629c8d061267b240f858b623d3b
Yoshimura Naoya <yesyes708@gmail.com>
2018-01-15 23:02:41 +0900

    update to

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,28 +1,45 @@
 // Gesture Set
-// export const GESTURE_SET = [
-//   {id: 0, name: 'air tap',        img: "./img/gestures/air_tap.jpg"},
-//   {id: 1, name: 'flick left',     img: "./img/gestures/flick_left.jpg"},
-//   {id: 2, name: 'flick right',    img: "./img/gestures/flick_right.jpg"},
-//   {id: 3, name: 'pinch in',       img: "./img/gestures/pinch_in.jpg"},
-//   {id: 4, name: 'pinch out',      img: "./img/gestures/pinch_out.jpg"},
-//   {id: 5, name: 'tap arm',        img: "./img/gestures/tap_arm.jpg"},
-//   {id: 6, name: 'tap left',       img: "./img/gestures/tap_left.jpg"},
-//   {id: 7, name: 'tap sensor',     img: "./img/gestures/tap_sensor.jpg"},
-//   {id: 8, name: 'tap right',      img: "./img/gestures/tap_right.jpg"},
-//   {id: 9, name: 'tap hand',      img: "./img/gestures/tap_hand.jpg"},
-// ]
+export const GESTURE_SET = [
+  {id: 1, name: 'flick left',     img: "./img/gestures/flick_left.jpg"},
+  {id: 2, name: 'flick right',    img: "./img/gestures/flick_right.jpg"},
+  {id: 3, name: 'pinch out',       img: "./img/gestures/pinch_in.jpg"},
+  {id: 4, name: 'pinch in',      img: "./img/gestures/pinch_out.jpg"},
+  {id: 5, name: 'tap arm',        img: "./img/gestures/tap_arm.jpg"},
+  {id: 6, name: 'tap left',       img: "./img/gestures/tap_left.jpg"},
+  {id: 7, name: 'tap sensor',     img: "./img/gestures/tap_sensor.jpg"},
+  {id: 8, name: 'tap right',      img: "./img/gestures/tap_right.jpg"},
+  {id: 9, name: 'tap hand',      img: "./img/gestures/tap_hand.jpg"},
+  {id: 11, name: 'flick left',     img: "./img/gestures/flick_left.jpg"},
+  {id: 12, name: 'flick right',    img: "./img/gestures/flick_right.jpg"},
+  {id: 13, name: 'pinch out',       img: "./img/gestures/pinch_in.jpg"},
+  {id: 14, name: 'pinch in',      img: "./img/gestures/pinch_out.jpg"},
+  {id: 15, name: 'tap arm',        img: "./img/gestures/tap_arm.jpg"},
+  {id: 16, name: 'tap left',       img: "./img/gestures/tap_left.jpg"},
+  {id: 17, name: 'tap sensor',     img: "./img/gestures/tap_sensor.jpg"},
+  {id: 18, name: 'tap right',      img: "./img/gestures/tap_right.jpg"},
+  {id: 19, name: 'tap hand',      img: "./img/gestures/tap_hand.jpg"},
+  {id: 21, name: 'flick left',     img: "./img/gestures/flick_left.jpg"},
+  {id: 22, name: 'flick right',    img: "./img/gestures/flick_right.jpg"},
+  {id: 23, name: 'pinch out',       img: "./img/gestures/pinch_in.jpg"},
+  {id: 24, name: 'pinch in',      img: "./img/gestures/pinch_out.jpg"},
+  {id: 25, name: 'tap arm',        img: "./img/gestures/tap_arm.jpg"},
+  {id: 26, name: 'tap left',       img: "./img/gestures/tap_left.jpg"},
+  {id: 27, name: 'tap sensor',     img: "./img/gestures/tap_sensor.jpg"},
+  {id: 28, name: 'tap right',      img: "./img/gestures/tap_right.jpg"},
+  {id: 29, name: 'tap hand',      img: "./img/gestures/tap_hand.jpg"},
+]
 
 // Objects
-export const GESTURE_SET = [
-  {id: 0, name: 'drill',        img: "./img/objects/drill.jpg"},
-  {id: 1, name: 'driver',     img: "./img/objects/driver.jpg"},
-  {id: 2, name: 'hair dryer',    img: "./img/objects/hair_dryer.jpg"},
-  {id: 3, name: 'hand cleaner',       img: "./img/objects/hand_cleaner.jpg"},
-  {id: 4, name: 'massager (fast)',      img: "./img/objects/massager_fast.jpg"},
-  {id: 5, name: 'massager (slow)',        img: "./img/objects/massager_slow.jpg"},
-  {id: 6, name: 'toothbrush',       img: "./img/objects/toothbrush.jpg"},
-  {id: 7, name: 'shaver',     img: "./img/objects/shaver.jpg"},
-  {id: 8, name: 'fun',      img: "./img/objects/fun.jpg"},
-  {id: 9, name: 'hand mixer',      img: "./img/objects/hand_mixer.jpg"},
-  {id:10, name: 'blender',      img: "./img/objects/blender.jpg"}
-]
+// export const GESTURE_SET = [
+//   {id: 0, name: 'drill',        img: "./img/objects/drill.jpg"},
+//   {id: 1, name: 'driver',     img: "./img/objects/driver.jpg"},
+//   {id: 2, name: 'hair dryer',    img: "./img/objects/hair_dryer.jpg"},
+//   {id: 3, name: 'hand cleaner',       img: "./img/objects/hand_cleaner.jpg"},
+//   {id: 4, name: 'massager (fast)',      img: "./img/objects/massager_fast.jpg"},
+//   {id: 5, name: 'massager (slow)',        img: "./img/objects/massager_slow.jpg"},
+//   {id: 6, name: 'toothbrush',       img: "./img/objects/toothbrush.jpg"},
+//   {id: 7, name: 'shaver',     img: "./img/objects/shaver.jpg"},
+//   {id: 8, name: 'fun',      img: "./img/objects/fun.jpg"},
+//   {id: 9, name: 'hand mixer',      img: "./img/objects/hand_mixer.jpg"},
+//   {id:10, name: 'blender',      img: "./img/objects/blender.jpg"}
+// ]
